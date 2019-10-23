acc167b7b311f964a2699bd27b2cea7c38e85ff3
Mark Hender <mhender@pivotal.io>
2019-10-23 16:09:48 +0100

    Make it tls insecure safe
    * This is for now!
    
    Signed-off-by: Mark Hender <mhender@pivotal.io>
    Co-authored-by: Mark Hender <mhender@pivotal.io>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
@@ -63,15 +64,24 @@ func (c harborCollector) Collect(ch chan<- prometheus.Metric) {
 	c.mux.Lock() // To protect metrics from concurrent collects
 	defer c.mux.Unlock()
 
-	domain := "192.168.99.101:30003"
-	client := http.DefaultClient
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := http.Client{
+		Transport:     tr,
+	}
+
+	domain := "https://192.168.99.100:30003"
 	var isUp int
-	resp, err := client.Get(fmt.Sprintf("https://%s", domain))
+	resp, err := client.Get(fmt.Sprintf(domain))
 	if err != nil {
+		fmt.Println("error")
+		fmt.Printf("message: %s\n", err.Error())
 		isUp = 0
 	} else if resp.StatusCode == 200 {
 		isUp = 1
 	}
+	fmt.Printf("status code: %d\n", resp.StatusCode)
 
 	fmt.Println(fmt.Sprintf("**MARK**isUp: %d **", isUp))
 
