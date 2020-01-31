afe10847a0f7bc28534ed8ee547abf23df331ee3
Mark Hender <mhender@pivotal.io>
2020-01-31 13:38:56 +0000

    Add failure metric test for harbor health
    [#169780056](https://www.pivotaltracker.com/story/show/169780056)

--- collector/harbor_health_dashboard_test.go
+++ collector/harbor_health_dashboard_test.go
@@ -24,29 +24,58 @@ var _ = Describe("HarborHealthDashboard", func() {
 	})
 
 	Context("When updating metrics", func() {
-		BeforeEach(func() {
-			downstream := &httpmock.MockHandler{}
-			downstream.On("Handle", "GET", "/", mock.Anything).Return(httpmock.Response{
-				Body:   []byte(`{"status": "healthy"}`),
-				Status: 200,
+		Context("When Harbor is up", func() {
+			BeforeEach(func() {
+				downstream := &httpmock.MockHandler{}
+				downstream.On("Handle", "GET", "/", mock.Anything).Return(httpmock.Response{
+					Body:   []byte(`{"status": "healthy"}`),
+					Status: 200,
+				})
+
+				s = httpmock.NewServer(downstream)
+				err := os.Setenv("REGISTRY_SERVER", s.URL())
+				Expect(err).ToNot(HaveOccurred())
 			})
 
-			s = httpmock.NewServer(downstream)
-			err := os.Setenv("REGISTRY_SERVER", s.URL())
-			Expect(err).ToNot(HaveOccurred())
+			It("Should send a metric of value 1", func() {
+				ch := make(chan prometheus.Metric, 1)
+				err := healthScraper.Update(ch)
+				Expect(err).ToNot(HaveOccurred())
+				expectedResult := prometheus.MustNewConstMetric(
+					collector.HarborHealthDashboardMetric,
+					prometheus.GaugeValue,
+					float64(1),
+				)
+				Eventually(ch).Should(Receive(Equal(expectedResult)))
+				s.Close()
+			})
 		})
 
-		It("Should return a const metric", func() {
-			ch := make(chan prometheus.Metric, 1)
-			err := healthScraper.Update(ch)
-			Expect(err).ToNot(HaveOccurred())
-			expectedResult := prometheus.MustNewConstMetric(
-				collector.HarborHealthDashboardMetric,
-				prometheus.GaugeValue,
-				float64(1),
-			)
-			Eventually(ch).Should(Receive(Equal(expectedResult)))
-			s.Close()
+		Context("When Harbor is down", func() {
+			BeforeEach(func() {
+				downstream := &httpmock.MockHandler{}
+				downstream.On("Handle", "GET", "/", mock.Anything).Return(httpmock.Response{
+					Body:   []byte(`{"status": "healthy"}`),
+					Status: 500,
+				})
+
+				s = httpmock.NewServer(downstream)
+				err := os.Setenv("REGISTRY_SERVER", s.URL())
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("Should send a metric of value 0", func() {
+				ch := make(chan prometheus.Metric, 1)
+				err := healthScraper.Update(ch)
+				Expect(err).ToNot(HaveOccurred())
+				expectedResult := prometheus.MustNewConstMetric(
+					collector.HarborHealthDashboardMetric,
+					prometheus.GaugeValue,
+					float64(0),
+				)
+				Eventually(ch).Should(Receive(Equal(expectedResult)))
+				s.Close()
+			})
 		})
 	})
 })
